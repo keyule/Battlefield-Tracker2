8ec9a69b9e349ddd8373abd477185f138063a71d
yule <keyule@gmail.com>
2024-06-24 03:45:31 +0800

    add spawn time

--- battlefield.py
+++ battlefield.py
@@ -6,6 +6,8 @@ from api_manager import ApiManager
 from telegram_bot import TelegramBot
 from prettytable import PrettyTable
 from datetime import datetime
+import pytz
+
 
 # Load environment variables
 load_dotenv()
@@ -15,27 +17,41 @@ REGION_MAP = {0: "Pirate", 1: "Cat", 2: "Wolf", 3: "Food"}
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_ALERTS_ENABLED = os.getenv('TELEGRAM_ALERTS_ENABLED', 'False') == 'True'
 SLEEP_TIME = 60  # Sleep time in seconds (5 minutes)
+TIMEZONE = pytz.timezone('Asia/Singapore')
 
 class Mob:
-    def __init__(self, mob_id, region, level):
+    def __init__(self, mob_id, region, level, spawnTime = "Unknown"):
         self.mob_id = mob_id
         self.region = region
         self.level = level
+        self.spawnTime = spawnTime
+
+    def set_time(self):
+        current_time = datetime.now(TIMEZONE)
+        self.spawnTime = current_time.strftime('%H:%M')
+
 
 class MobList:
     def __init__(self):
         self.mobs = []
-        self.previous_ids = []
+        self.previous_mobs = []
 
     def update_mobs(self, new_mobs):
-        self.previous_ids = [mob.mob_id for mob in self.mobs]
-        self.mobs = new_mobs
-
-    def get_new_mobs(self):
-        if not self.previous_ids:
+        if not self.previous_mobs:
+            self.mobs = new_mobs
             return []
-        new_mobs = [mob for mob in self.mobs if mob.mob_id not in self.previous_ids]
-        return new_mobs
+
+        self.previous_mobs = self.mobs
+        new_mobs_list = []
+
+        for mob in new_mobs:
+            if mob.mob_id not in {m.mob_id for m in self.previous_mobs}:
+                mob.set_time()
+                new_mobs_list.append(mob)
+
+        self.mobs = [mob for mob in self.mobs if mob.mob_id in {m.mob_id for m in new_mobs}] + new_mobs_list
+
+        return new_mobs_list
 
     def get_current_mobs(self):
         return self.mobs
@@ -44,10 +60,10 @@ class UI:
     @staticmethod
     def print_battlefield_info(mob_list):
         table = PrettyTable()
-        table.field_names = ["ID", "Region", "Level"]
+        table.field_names = ["ID", "Region", "Level", "SpawnTime"]
 
         for mob in mob_list.mobs:
-            table.add_row([mob.mob_id, mob.region, mob.level])
+            table.add_row([mob.mob_id, mob.region, mob.level, mob.spawnTime])
 
         print(table)
 
@@ -57,22 +73,16 @@ class UI:
 
 class Alert:
     @staticmethod
-    async def alert_for_new_mobs(new_mobs, telegram_bot):
+    async def alert_for_new_mobs(new_mobs, telegram_bot, mob_type=""):
         for mob in new_mobs:
-            alert_message = (
-                f"New Mob Spawned! Level: {mob.level} region: {mob.region}"
-            )
-
-            if TELEGRAM_ALERTS_ENABLED:
-                await telegram_bot.send_alert(alert_message)
-
-            UI.print_alert_message(alert_message)
-
-    async def alert_for_new_mobs2(new_mobs, telegram_bot):
-        for mob in new_mobs:
-            alert_message = (
-                f"New WORLD Mob Spawned! Level: {mob.level} region: {mob.region}"
-            )
+            if mob_type:
+                alert_message = (
+                    f"New {mob_type.upper()} Mob Spawned! Level: {mob.level} region: {mob.region}"
+                )
+            else:
+                alert_message = (
+                    f"New Mob Spawned! Level: {mob.level} region: {mob.region}"
+                )
 
             if TELEGRAM_ALERTS_ENABLED:
                 await telegram_bot.send_alert(alert_message)
@@ -89,50 +99,35 @@ class Battlefield:
         self.world_mob_list = world_mob_list
         self.timer = 0
 
+    async def process_mobs(self, get_data_func, mob_list, mob_type=""):
+        data = get_data_func()
+        new_mobs = []
+        for region in data["regions"]:
+            region_name = REGION_MAP.get(region['region'], "Unknown")
+            for battlefield in region["battlefields"]:
+                mob = Mob(
+                    mob_id=battlefield['id'],
+                    region=region_name,
+                    level=battlefield['level'],
+                )
+                new_mobs.append(mob)
+
+        new_mobs_list = mob_list.update_mobs(new_mobs)
+
+        if new_mobs_list:
+            UI.print_battlefield_info(mob_list)
+            await Alert.alert_for_new_mobs(new_mobs_list, self.telegram_bot, mob_type)
+
     async def run(self):
         try:
             while self.running:
-                data = self.api_manager.get_battlefields()
-                new_mobs = []
-                for region in data["regions"]:
-                    region_name = REGION_MAP.get(region['region'], "Unknown")
-                    for battlefield in region["battlefields"]:
-                        mob = Mob(
-                            mob_id=battlefield['id'],
-                            region=region_name,
-                            level=battlefield['level'],
-                        )
-                        new_mobs.append(mob)
+                await self.process_mobs(self.api_manager.get_battlefields, self.mob_list)
+                await self.process_mobs(self.api_manager.get_world_battlefields, self.world_mob_list, "WORLD")
 
-                self.mob_list.update_mobs(new_mobs)
-
-                #UI.print_battlefield_info(self.mob_list)
-                new_mobs = self.mob_list.get_new_mobs()
-                if new_mobs:
-                    await Alert.alert_for_new_mobs(new_mobs, self.telegram_bot)
-
-                data2 = self.api_manager.get_world_battlefields()
-                new_mobs2 = []
-                for region in data2["regions"]:
-                    region_name = REGION_MAP.get(region['region'], "Unknown")
-                    for battlefield in region["battlefields"]:
-                        mob = Mob(
-                            mob_id=battlefield['id'],
-                            region=region_name,
-                            level=battlefield['level'],
-                        )
-                        new_mobs2.append(mob)
-
-                self.world_mob_list.update_mobs(new_mobs2)
-
-                #UI.print_battlefield_info(self.world_mob_list)
-                new_mobs2 = self.world_mob_list.get_new_mobs()
-                if new_mobs2:
-                    await Alert.alert_for_new_mobs2(new_mobs2, self.telegram_bot)
 
                 if self.timer > 5:
                     self.timer = 0
-                    time = datetime.now().strftime("%H:%M:%S")
+                    time = datetime.now(TIMEZONE).strftime("%H:%M:%S")
                     print("Last Updated:", time)
 
                 self.timer = self.timer + 1
@@ -140,8 +135,11 @@ class Battlefield:
         except asyncio.CancelledError:
             self.stop()
 
+
     def stop(self):
         self.running = False
+
+
 
 async def main():
     parser = argparse.ArgumentParser(description='Run the battlefield monitoring script.')
@@ -152,7 +150,7 @@ async def main():
     world_mob_list = MobList()
 
     if TELEGRAM_ALERTS_ENABLED:
-        telegram_bot = TelegramBot(TELEGRAM_TOKEN)
+        telegram_bot = TelegramBot(TELEGRAM_TOKEN, mob_list)
     else:
         telegram_bot = None
 
